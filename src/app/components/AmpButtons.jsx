6009235e4f0cb02ea0e85db983084b19392ad373
Barmington Chuku <barmingtonk@gmail.com>
2024-12-17 12:38:05 +0000

    update

--- src/app/components/AmpButtons.jsx
+++ src/app/components/AmpButtons.jsx
@@ -1,14 +1,15 @@
-'use client';
+"use client";
 
 import { useFormStatus } from "react-dom";
 import clsx from "clsx";
+import Image from "next/image";
 import {
   TbArrowBigDown,
   TbArrowBigDownFilled,
   TbArrowBigUp,
   TbArrowBigUpFilled,
-} from 'react-icons/tb';
-import { FaSpinner } from 'react-icons/fa';
+} from "react-icons/tb";
+import { FaSpinner } from "react-icons/fa";
 
 export default function AmpButtons({ amp, damp, amps, existingAmps }) {
   const { pending, data, method, action } = useFormStatus();
@@ -18,15 +19,15 @@ export default function AmpButtons({ amp, damp, amps, existingAmps }) {
         {existingAmps?.vote === 1 ? (
           <TbArrowBigUpFilled
             size={24}
-            className={clsx('hover:text-orange-600', {
-              'text-pink-300': existingAmps?.vote === 1,
+            className={clsx("hover:text-orange-600", {
+              "text-pink-300": existingAmps?.vote === 1,
             })}
           />
         ) : (
           <TbArrowBigUp
             size={24}
-            className={clsx('hover:text-orange-600', {
-              'text-pink-300': existingAmps?.vote === 1,
+            className={clsx("hover:text-orange-600", {
+              "text-pink-300": existingAmps?.vote === 1,
             })}
           />
         )}
@@ -44,15 +45,15 @@ export default function AmpButtons({ amp, damp, amps, existingAmps }) {
         {existingAmps?.vote === -1 ? (
           <TbArrowBigDownFilled
             size={24}
-            className={clsx('hover:text-blue-600', {
-              'text-blue-300': existingAmps?.vote === -1,
+            className={clsx("hover:text-blue-600", {
+              "text-blue-300": existingAmps?.vote === -1,
             })}
           />
         ) : (
           <TbArrowBigDown
             size={24}
-            className={clsx('hover:text-blue-600', {
-              'text-blue-300': existingAmps?.vote === -1,
+            className={clsx("hover:text-blue-600", {
+              "text-blue-300": existingAmps?.vote === -1,
             })}
           />
         )}
