87f1786ba422e24f4f10b9bd41920bd06e0a543d
Bambam <exp.ai@outlook.com>
2024-12-18 19:23:32 +0000

    package funds and navigation change

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,13 +5,14 @@ import "./globals.css";
 
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "./components/Header";
+import Navigation from "./components/Navigation";
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
         <body className="grid grid-rows-[auto_1fr_auto] h-screen">
-          <Header className="bg-white shadow-md w-full max-height-20 z-50" />
+          <Navigation className="bg-white shadow-md w-full max-height-20 z-50" />
 
           {/*Main Content*/}
           <main className="bg-purple-100 py-6 px-4 rounded-lg shadow-md min-w-80 max-w-lg mx-auto overflow-y-auto">
